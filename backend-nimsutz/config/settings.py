7a91d6cbdb1d1be48cf925a27e9659d6e97ec652
CrisKroos25 <141697280+CrisKroos25@users.noreply.github.com>
2026-09-18 02:11:51 -0600

    chore: modifying the bucket file configuration

--- backend-nimsutz/config/settings.py
+++ backend-nimsutz/config/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
-load_dotenv()
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- backend-nimsutz/crear_bucket.py
+++ backend-nimsutz/crear_bucket.py
@@ -2,12 +2,14 @@ import boto3
 from botocore.client import Config
 import os
 
+BUCKET_NAME = os.environ["MINIO_BUCKET_NAME"]
+
 # Cliente interno: para operaciones que hace el backend mismo (crear bucket, verificar, etc.)
 s3_interno = boto3.client(
     "s3",
     endpoint_url=os.environ.get("MINIO_ENDPOINT"),  # http://minio:9000
-    aws_access_key_id="nimsutz_admin",
-    aws_secret_access_key="nimsutz_admin_pass",
+    aws_access_key_id=os.environ["MINIO_ACCESS_KEY"],
+    aws_secret_access_key=os.environ["MINIO_SECRET_KEY"],
     config=Config(signature_version="s3v4"),
 )
 
@@ -15,10 +17,10 @@ s3_interno = boto3.client(
 s3_publico = boto3.client(
     "s3",
     endpoint_url=os.environ.get("MINIO_PUBLIC_ENDPOINT"),  # http://localhost:9000
-    aws_access_key_id="nimsutz_admin",
-    aws_secret_access_key="nimsutz_admin_pass",
+    aws_access_key_id=os.environ["MINIO_ACCESS_KEY"],
+    aws_secret_access_key=os.environ["MINIO_SECRET_KEY"],
     config=Config(signature_version="s3v4"),
 )
 
-s3_publico.create_bucket(Bucket="nimsutz-storage")
-print("Bucket creado correctamentea")
+s3_interno.create_bucket(Bucket=BUCKET_NAME)
+print("Bucket creado correctamente")
